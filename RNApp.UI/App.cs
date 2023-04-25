45fd7f216b74e6e10d86323fe7bbb891beffa127
burningbridges9 <talipov9@gmail.com>
2023-04-26 01:40:31 +0300

    Add project files.

---
+++ RNApp.UI/App.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace RNApp.UI
+{
+    public class App : Application
+    {
+        readonly MainWindow mainWindow;
+
+        public App(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
+    }
+}
